a66ddb8a22aa557b229cfc4ca272c39b992a42e0
dub-flow <dub-flow@secret.local>
2023-12-25 14:26:15 +0100

    Improved logging for challenge 2

--- challenge-02/main.js
+++ challenge-02/main.js
@@ -4,20 +4,26 @@ const axios = require('axios');
 const app = express();
 
 app.get('/profile', (req, res) => {
+    console.log('Received request for /profile');
     // Simulated profile data
     const profileData = {
         name: 'John Doe',
         role: 'Developer'
     };
     res.json(profileData);
+    console.log('Sent profile data response');
 });
 
 app.get('/fetch-data', async (req, res) => {
     const url = req.query.url;
+    console.log(`Received request for /fetch-data with URL: ${url}`);
+    
     try {
         const response = await axios.get(url);
         res.send(response.data);
+        console.log(`Data fetched and sent for URL: ${url}`);
     } catch (error) {
+        console.error(`Error fetching data from URL: ${url}`, error);
         res.status(500).send('Error fetching data');
     }
 });
