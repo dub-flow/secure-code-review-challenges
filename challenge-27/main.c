31c6570c866fa1e05cae90a1fd6993217b2fd753
dub-flow <89262088+dub-flow@users.noreply.github.com>
2025-08-18 18:49:13 +0200

    Simplified challenge 27

--- challenge-27/main.c
+++ challenge-27/main.c
@@ -5,37 +5,20 @@
 
 #define FIXED_PRICE 1  // Price per item (in euros)
 
-void apply_tax(int *total, float tax_percentage) {
-    if (tax_percentage < 0 || tax_percentage > 100) {
-        printf("Error: Invalid tax percentage.\n");
-        exit(1);
-    }
-    *total += (*total * tax_percentage) / 100; 
-}
-
-void calculate_total(int quantity, int price_per_item, float tax_percentage) {
+void calculate_total(int quantity, int price_per_item) {
     int total = quantity * price_per_item;
 
-    if (tax_percentage > 0) {
-        apply_tax(&total, tax_percentage);
-    }
-
-    printf("Total price before tax: %d euros\n", quantity * price_per_item);
-    printf("Total price after tax: %d euros\n", total);
+    printf("Total price: %d euros\n", total);
 }
 
 int main() {
     int quantity;
-    float tax_percentage;
 
     printf("Enter quantity of items: ");
     scanf("%d", &quantity);
     printf("Your entered quantity: %d\n", quantity);
-
-    printf("Enter tax percentage: ");
-    scanf("%f", &tax_percentage);
-
-    calculate_total(quantity, FIXED_PRICE, tax_percentage);
+    printf("Fixed price per item is: %d euro\n", FIXED_PRICE);
+    calculate_total(quantity, FIXED_PRICE);
 
     return 0;
 }
